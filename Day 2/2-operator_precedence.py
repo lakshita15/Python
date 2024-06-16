148dbf6be7c44cf6202b3ecea0168d067e0d9cb8
lakshita15 <lakshitamahajan2001@gmail.com>
2024-06-16 19:27:25 -0400

    day 2

---
+++ Day 2/2-operator_precedence.py
@@ -0,0 +1,6 @@
+# parenthesis or brackets override order
+# 1 exponential
+# 2 multiplication
+# 3 addition/subtraction
+# print(10-4/2 + 2*2 + 2**2)
+print((2+3)*10-3)
---
+++ Day 2/3-MathFunctions.py
@@ -0,0 +1,20 @@
+"""
+x=2.9
+# round it
+x=round(x)
+print(x) #gives 3
+
+#####*************#######
+
+#return a positive number we use absolute
+print(abs(-2.9))
+
+#####*************#######
+
+# these all of handful, builtin math functions, but if I want to write more complex functions, I need to import the math module => seperate file with reusable code
+"""
+
+import math
+
+print(math.ceil(2.9))
+print(math.floor(2.9))
